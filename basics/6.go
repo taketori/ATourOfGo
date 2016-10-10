c1054e716d69507a9701e5fd502455688ba28c40
taketori <dm1@x10d.jp>
2016-10-11 00:32:34 +0900

    tried bare return

--- basics/6.go
+++ basics/6.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+func swap(x, y string) (newX string, newY string) {
+	newX = y
+	newY = x
+	return
 }
 
 func main() {
