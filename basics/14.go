e081b8f088c8e6997060fc040605cbf0fece380e
taketori <dm1@x10d.jp>
2016-10-15 20:52:42 +0900

    tried changing the initial value of v, observed how its type affected and wrote them down in code.

--- basics/14.go
+++ basics/14.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	v := 42
+	//v := 42	// int
+	//v := 3.142	// float64
+	//v := 0.867 + 0.5i	// complex128
+	//v := "abcd" // string
+	//v := []rune("abcd") // rune []int32
+	v := '❤' // rune int32
 
 	fmt.Printf("%v is of type %T\n", v, v)
 }
