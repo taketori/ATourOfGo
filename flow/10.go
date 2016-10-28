05366369096e530ef70024bc5703f25f5b138cf8
taketori <dm1@x10d.jp>
2016-10-28 18:59:30 +0900

    added f() to see if f() is called or not and saw not calling.
    anyway, I think the sample is not good as it's not clear that "case today + 1" etc is evaluated or not when today == time.Saturday.

--- flow/10.go
+++ flow/10.go
@@ -5,6 +5,12 @@ import (
 	"time"
 )
 
+func f() (r time.Weekday) {
+	fmt.Println("this will not be called.")
+	r = time.Thursday
+	return
+}
+
 func main() {
 	fmt.Println("When's Saturday?")
 	today := time.Now().Weekday()
@@ -15,6 +21,8 @@ func main() {
 		fmt.Println("Tommorow")
 	case today + 2:
 		fmt.Println("In Two days.")
+	case f():
+		fmt.Println("It's Thursday.")
 	default:
 		fmt.Println("Too far away.")
 	}
