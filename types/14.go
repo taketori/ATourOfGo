4d91cbcfa81ee4c5f5a4ec41bf2a49646d2423a9
taketori <dm1@x10d.jp>
2017-06-04 16:24:21 +0900

    they drew.

--- types/14.go
+++ types/14.go
@@ -17,6 +17,9 @@ func main() {
 	board[1][2] = "X"
 	board[1][0] = "O"
 	board[0][2] = "X"
+	board[0][1] = "O"
+	board[1][1] = "X"
+	board[2][0] = "O"
 
 	for i := 0; i < len(board); i++ {
 		fmt.Printf("%s\n", strings.Join(board[i], " "))
