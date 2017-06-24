a7897abb27bc55a09f675bdedd1c17816a3da266
taketori <dm1@x10d.jp>
2017-06-24 19:32:35 +0900

    change Sprintf's argument from %v to %g
    %v shows stack overflow in case Sqrt  argument is negative.

--- methods/20.go
+++ methods/20.go
@@ -8,7 +8,7 @@ import (
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %v", e)
+	return fmt.Sprintf("cannot Sqrt negative number: %g", e)
 }
 
 func Sqrt(x float64) (float64, error) {
